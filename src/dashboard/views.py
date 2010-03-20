c3ccddd0ed519fc792d71a3b7b8571fd4429f45e
John Paulett <john@paulett.org>
2010-03-19 19:12:44 -0500

    Make the django apps' versions available in the template.

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,6 +1,9 @@
 from annoying.decorators import render_to
+from debug_toolbare.debug.version import DebugVersion
 from stream.models import Item
 
 @render_to('dashboard/index.html')
 def index(request):
-    return {'stream': Item.objects.all().order_by('-time')[:20]}
+    
+    return {'stream': Item.objects.all().order_by('-time')[:20],
+            'versions': DebugVersion()}
