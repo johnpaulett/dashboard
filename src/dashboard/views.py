cca07facd09af9b667eb97a6afce4e33830d4a83
John Paulett <john@paulett.org>
2010-03-20 23:54:14 -0500

    Add a date column to the stream listing so that news items are visibly grouped together by date.

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,9 +1,22 @@
 from annoying.decorators import render_to
+from django.utils.datastructures import SortedDict
 from monit.models import Server
 from stream.models import Item
 
 @render_to('dashboard/index.html')
 def index(request):
-    
-    return {'stream': Item.objects.all().order_by('-time')[:20],
+    stream = group_by_field(Item.objects.all().order_by('-time')[:20], lambda x: x.time.date())
+    return {'stream': stream,
             'monit_servers': Server.objects.all()}
+
+
+# TODO consider moving to a utils module
+def group_by_field(queryset, field):
+    result = SortedDict()
+    for item in queryset:
+        if callable(field):
+            key = field(item)
+        else:
+            key = getattr(item, field)
+        result.setdefault(key, []).append(item)
+    return result
