a127f31fb1bffd157b0c991c622e1af2b7d25749
John Paulett <john@paulett.org>
2010-03-22 22:00:12 -0500

    Hide widgets on double-click of widget header.

--- src/static/js/dashboard.js
+++ src/static/js/dashboard.js
@@ -1,12 +1,19 @@
 $(function() {
+    
+    $('.widget h4').dblclick(function(obj){ 
+        $(obj.target).parents('.widget').hide('puff');
+    });
+
     /* detect HTML5 & CSS3 support */
     if (!Modernizr.borderradius || !Modernizr.fontface) {
 	
-	var $dialog = $('<div></div>')
+	var dialog = $('<div></div>')
 	    .html('Your browser does not support the awesome features of <a href="http://diveintohtml5.org/">HTML5</a> & CSS3. Feel free to continue, but you will likely have a degraded experience.  Consider upgrading to <a href="http://www.google.com/chrome">Chrome</a> or <a href="http://www.mozilla.com/firefox/">Firefox</a>.')
 	    .dialog({
 		title: 'Unsupported Browser',
 		modal: true
 	    });
     }
+   
+
 });
